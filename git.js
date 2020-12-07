c931fb2f397e92d0aab3c5ab3c0472cba97a8160
Arqum0206 <75626518+Arqum0206@users.noreply.github.com>
2020-12-07 20:36:52 +0500

    Add files via upload

---
+++ git.js
@@ -0,0 +1,27 @@
+// var its = new Date();
+// console.log(its);
+// var its = new Date();
+// console.log(its.getDate());
+
+// var its = new Date();
+// console.log(its.getMonth());
+
+// var its = new Date();
+// console.log(its.getFullYear());
+
+// var its = new Date()
+// its.setFullYear(1992);
+// console.log((its.getFullYear(its)));
+
+// var its = new Date()
+// its.setMonth(07);
+// console.log((its.getMonth(its)));
+
+// var its = new Date()
+// its.setMinutes(12);
+// console.log((its.getMinutes(its)));
+
+var its = new Date()
+its.setFullYear();
+console.log((its.getFullYear()));
+
