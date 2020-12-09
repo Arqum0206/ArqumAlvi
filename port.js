d764d36ba12e4b03838fc76876cf4255918a367f
Arqum0206 <75626518+Arqum0206@users.noreply.github.com>
2020-12-09 20:28:53 +0500

    Add files via upload

---
+++ port.js
@@ -0,0 +1,61 @@
+// const express = require (`express`)
+// const app = express();
+// const port = 3001;
+
+// app.get('/', (req, res) =>{
+//     res.send(`Hello world`)
+//     console.log("hi");
+// });
+
+// app.listen(port, () =>{
+//     console.log(`Example app listening on port ${port}!`);
+// });
+// const express = require (`express`)
+// const app = express();
+// const port = 3001;
+
+// app.get('/home', (req, res) =>{
+//     res.send(`Hello world`)
+//     console.log("hi");
+// });
+
+// app.listen(port, () =>{
+//     console.log(`Example app listening on port ${port}!`);
+// });
+// const express = require (`express`)
+// const app = express();
+// const port = 3001;
+
+// app.get('/Page01', (req, res) =>{
+//     res.send(`Hello world`)
+//     console.log("hi");
+// });
+
+// app.listen(port, () =>{
+//     console.log(`Example app listening on port ${port}!`);
+// });
+
+// const express = require (`express`)
+// const app = express();
+// const port = 3001;
+
+// app.get('/Page02', (req, res) =>{
+//     res.send(`Hello world`)
+//     console.log("hey");
+// });
+
+// app.listen(port, () =>{
+//     console.log(`Example app listening on port ${port}!`);
+// });
+const express = require (`express`)
+const app = express();
+const port = 3001;
+
+app.get('/Page03', (req, res) =>{
+    res.send(`Salamoualaikum`)
+    console.log("Walaikumsalam");
+});
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}!`);
+});
